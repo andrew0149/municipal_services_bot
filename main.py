8ff20bae146305e3f0deca1a504ef473f1b1992d
andrew0149 <andrew-m0r0z@mail.ru>
2023-05-27 20:25:00 +0500

    Made postgre.py actually interact with DB Added user deletion dialogue

--- main.py
+++ main.py
@@ -10,8 +10,51 @@ import postgre as Postgre
 import actions as Action
 
 
+def get_users_page_keyboard(callback, page_n):
+	users_list = Postgre.get_users()
+	keyboard = types.InlineKeyboardMarkup()
+
+	for user in users_list[page_n * 10:min((page_n + 1) * 10, len(users_list))]:
+		user_id, user_phone, _, _, _ = user
+		keyboard.add(
+			types.InlineKeyboardButton(
+				text = f'{user_id}) {user_phone}',
+				callback_data = f'{callback},{user_id}'),
+			row_width = 1)
+	
+	navigation_buttons = []
+	
+	if page_n != 0:
+		navigation_buttons.append(
+			types.InlineKeyboardButton(
+				text = '⬅',
+				callback_data = f'get_users_page,{page_n - 1},{callback}'))
+	if (page_n + 1) * 10 < len(users_list):
+		navigation_buttons.append(
+			types.InlineKeyboardButton(
+				text = '➡',
+				callback_data = f'get_users_page,{page_n + 1},{callback}'))
+	
+	if len(navigation_buttons) != 0:
+		keyboard.add(*navigation_buttons, row_width = 2)
+	
+	keyboard.add(
+		types.InlineKeyboardButton(
+			text = '❌ Отменить действие',
+			callback_data = 'cancel'))
+
+	return keyboard
+
+async def remove_inline_reply_markup(call):
+	await bot.edit_message_reply_markup(
+		call.message.chat.id,
+		call.message.message_id,
+		reply_markup = None)
+
+# Инициализация бота
 bot = AsyncTeleBot(API_TOKEN)
 
+# Обработка команд /start и /help
 @bot.message_handler(commands=['start', 'help'])
 async def send_welcome(message):
 	if message.text in ('/start', '/help'):
@@ -26,6 +69,7 @@ async def send_welcome(message):
 
 	user_id = Postgre.get_user_id_for_chat(message.chat.id)
 
+	# Если user_id не вернулся, заставляем пользователя авторизоваться 
 	if user_id is None:
 		phone_send_button = types.KeyboardButton(text='Отправить номер телефона', request_contact=True)
 		markup.add(phone_send_button)
@@ -35,6 +79,7 @@ async def send_welcome(message):
 			reply_markup=markup)
 		return
 	
+	# админу крепим админские кнопки
 	if Postgre.is_admin(user_id):
 		for row in Action.ADMIN_MARKUP:
 			markup.add(
@@ -42,6 +87,7 @@ async def send_welcome(message):
 					lambda index: Action.ADMIN_ACTIONS[index], 
 					row), 
 				row_width=len(row))
+	# а пользователю - пользовательские
 	else:
 		for row in Action.USER_MARKUP:
 			markup.add(
@@ -50,11 +96,13 @@ async def send_welcome(message):
 					row), 
 				row_width=len(row))
 	
+	# если пользователь вызывал не справку, здороваемся
 	if message.text != '/help':
 		await bot.send_message(message.chat.id,
 			text = f'Привет, {message.chat.first_name}!',
 			reply_markup = markup)
 
+# деавторизация по команде /logout
 @bot.message_handler(commands=['logout'])
 async def deauthorize_user(message):
 	Postgre.unlink_chat_from_user(message.chat.id)
@@ -63,6 +111,7 @@ async def deauthorize_user(message):
 		text=f'Вы были деавторизованы!',
 		reply_markup=types.ReplyKeyboardRemove())
 
+# обработка контактов, которая должна происходить только при и для авторизации
 @bot.message_handler(content_types=['contact'])
 async def authorize_user(message):
 	if message.from_user.id == message.contact.user_id:
@@ -82,6 +131,7 @@ async def authorize_user(message):
 			text='Это не ваш контакт! Используйте свой контакт для авторизации.',
 			reply_markup=markup)
 
+# Обработка кнопок
 @bot.message_handler(content_types=['text'])
 async def process_message(message):
 	user_id = Postgre.get_user_id_for_chat(message.chat.id)
@@ -95,16 +145,77 @@ async def process_message(message):
 
 		state = Postgre.get_chat_state(message.chat.id)
 
-		if message.text in Action.ADMIN_ACTIONS:
+		# Создание клавиатуры для отмены действий
+		cancel_keyboard = types.InlineKeyboardMarkup()
+		cancel_keyboard.add(
+			types.InlineKeyboardButton(
+				text = '❌ Отменить действие',
+				callback_data = 'cancel'))
+
+		if state is not None and state != '':
+
+			# Обработка состояний
+			if state.startswith('ADD_USER'):
+				if state.startswith('ADD_USER_PHONE'):
+					if phone_number_regexp.match(message.text):
+						Postgre.set_chat_state(message.chat.id, 'ADD_USER_IS_ADMIN,' + message.text)
+						await bot.send_message(
+							message.chat.id, 
+							text = 'Должен ли пользователь быть администратором? [да/нет]',
+							reply_markup = cancel_keyboard)
+					else:
+						await bot.send_message(message.chat.id, text='Неверный формат номера...')
+				if state.startswith('ADD_USER_IS_ADMIN'):
+					parsed_phone = state.split(',')[1]
+	
+					if message.text.lower() in ('да', 'yes', 'д', 'y'):
+						new_user_is_admin = 'true'
+					elif message.text.lower() in ('нет', 'no', 'н', 'n'):
+						new_user_is_admin = 'false'				
+					else:
+						await bot.send_message(message.chat.id, text='Неверный ввод...')
+						return
+	
+					Postgre.set_chat_state(message.chat.id, ','.join(('ADD_USER_DISCOUNT', parsed_phone, new_user_is_admin)))
+					discounts_list = Postgre.get_discounts_list()
+					discounts_list_str = '\n'.join((f'{discount_id}) {discount_name}' for discount_id, discount_name in discounts_list))
+					await bot.send_message(
+						message.chat.id, 
+						text='Введите номер тарифа для нового пользователя:\n' + discounts_list_str,
+						reply_markup = cancel_keyboard)
+				if state.startswith('ADD_USER_DISCOUNT'):
+					_, parsed_phone, parsed_new_user_is_admin = state.split(',')
+					if message.text in (str(discount_id) for discount_id, _ in Postgre.get_discounts_list()):
+						Postgre.add_user(parsed_phone, parsed_new_user_is_admin, message.text)
+						await bot.send_message(
+							message.chat.id,
+							text = f'Добавлен пользователь с номером телефона {parsed_phone}, ' +
+								f'тарифом {int(message.text)}, ' +
+								('не' if parsed_new_user_is_admin == 'false' else '') + ' являющийся администратором.')
+						Postgre.set_chat_state(message.chat.id, '')
+					else:
+						await bot.send_message(message.chat.id, text='Неверный номер тарифа...')
+			if state.startswith('DEL_USER'):
+				await bot.send_message(
+					message.chat.id,
+					text = 'Завершите или отмените удаление перед выполнением других действий')
+
+		# Обработка админских кнопок
+		elif message.text in Action.ADMIN_ACTIONS:
 			print(f'ADMIN ACTION --- chat_id={message.chat.id} action="{message.text}"')
 			match message.text:
 				case Action.ADD_USER:
 					await bot.send_message(
 						message.chat.id,
-						text = 'Введите номер телефона для которого хотите добавить аккаунт в формате "+79123456789"')
+						text = 'Введите номер телефона для которого хотите добавить аккаунт в формате "+79123456789"',
+						reply_markup = cancel_keyboard)
 					Postgre.set_chat_state(message.chat.id, 'ADD_USER_PHONE')
 				case Action.DEL_USER:
-					pass
+					await bot.send_message(
+						message.chat.id,
+						text = 'Выберите пользователя, которого хотите удалить:',
+						reply_markup = get_users_page_keyboard('delete_user', 0))
+					Postgre.set_chat_state(message.chat.id, 'DEL_USER')
 				case Action.UPDATE_USER:
 					pass
 				case Action.ADD_TARIFF:
@@ -118,45 +229,11 @@ async def process_message(message):
 				case Action.DEL_DEVICE:
 					pass
 
-		# Одно большое создание пользователя
-		elif state.startswith('ADD_USER_PHONE'):
-			if phone_number_regexp.match(message.text):
-				Postgre.set_chat_state(message.chat.id, 'ADD_USER_IS_ADMIN,' + message.text)
-				await bot.send_message(message.chat.id, text='Должен ли пользователь быть администратором? [да/нет]')
-			else:
-				Postgre.set_chat_state(message.chat.id, '')
-				await bot.send_message(message.chat.id, text='Неверный формат номера...')
-		elif state.startswith('ADD_USER_IS_ADMIN'):
-			parsed_phone = state.split(',')[1]
-			if message.text.lower() in ('да', 'yes', 'д', 'y'):
-				new_user_is_admin = 'true'
-			elif message.text.lower() in ('нет', 'no', 'н', 'n'):
-				new_user_is_admin = 'false'				
-			else:
-				await bot.send_message(message.chat.id, text='Неверный ввод...')
-				Postgre.set_chat_state(message.chat.id, '')
-				return
-			Postgre.set_chat_state(message.chat.id, ','.join(('ADD_USER_DISCOUNT', parsed_phone, new_user_is_admin)))
-			discounts_list = Postgre.get_discounts_list()
-			discounts_list_str = '\n'.join((f'{discount_id}) {discount_name}' for discount_id, discount_name in discounts_list))
-			await bot.send_message(message.chat.id, text='Введите номер тарифа для нового пользователя:\n' + discounts_list_str)
-		elif state.startswith('ADD_USER_DISCOUNT'):
-			_, parsed_phone, parsed_new_user_is_admin = state.split(',')
-			if message.text in (str(discount_id) for discount_id, _ in Postgre.get_discounts_list()):
-				Postgre.add_user(parsed_phone, parsed_new_user_is_admin, message.text)
-				await bot.send_message(
-					message.chat.id,
-					text = f'Добавлен пользователь с номером телефона {parsed_phone}, ' +
-						f'тарифом {int(message.text)}, ' +
-						('не' if parsed_new_user_is_admin == 'false' else '') + ' являющийся администратором.')
-			else:
-				await bot.send_message(message.chat.id, text='Неверный номер тарифа...')
-			Postgre.set_chat_state(message.chat.id, '')
-
 		else:
 			await send_error(message)
  	
 	else:
+		# Обработка пользовательских кнопок
 		if message.text in Action.USER_ACTIONS:
 			print(f'USER ACTION --- chat_id={message.chat.id} action="{message.text}"')
 			match message.text:
@@ -173,11 +250,58 @@ async def process_message(message):
 		else:
 			await send_error(message)
 
+# Обработка кнопки отмены
+@bot.callback_query_handler(func=lambda call: call.data == 'cancel')
+async def cancel_operation(call):
+	Postgre.set_chat_state(call.message.chat.id, '')
+	await remove_inline_reply_markup(call)
+	await bot.send_message(
+		call.message.chat.id,
+		text = 'Операция отменена')
+
+# Обработка перелистывания страниц с пользователями
+@bot.callback_query_handler(func=lambda call: call.data.split(',')[0] == 'get_users_page')
+async def get_new_users_page(call):
+	page_n = int(call.data.split(',')[1])
+	callback = call.data.split(',')[2]
+	await bot.edit_message_reply_markup(
+		call.message.chat.id,
+		call.message.message_id,
+		reply_markup = get_users_page_keyboard(callback, page_n))
+
+# Запрос подтверждения на удаление пользователя
+@bot.callback_query_handler(func=lambda call: call.data.split(',')[0] == 'delete_user')
+async def delete_user(call):
+	user_id = call.data.split(',')[1]
+	await remove_inline_reply_markup(call)
+	choice_keyboard = types.InlineKeyboardMarkup()
+	choice_keyboard.add(
+		types.InlineKeyboardButton(
+			text = '✔ Да',
+			callback_data = f'delete_user_confirmed,{user_id}'),
+		types.InlineKeyboardButton(
+			text = '❌ Нет',
+			callback_data = 'cancel'),
+		row_width = 2)
+	await bot.send_message(
+		call.message.chat.id,
+		text = 'Вы уверены, что хотите удалить пользователя?',
+		reply_markup = choice_keyboard)
+
+# Удаление пользователя подтверждено - УДАЛЯЕМ!
+@bot.callback_query_handler(func=lambda call: call.data.split(',')[0] == 'delete_user_confirmed')
+async def delete_user_confirmed(call):
+	await remove_inline_reply_markup(call)
+	user_id = call.data.split(',')[1]
+	Postgre.delete_user(user_id)
+	Postgre.set_chat_state(call.message.chat.id, '')
+	await bot.send_message(
+		call.message.chat.id,
+		text = f'Пользователь с ID {user_id} удалён')
 
 @bot.message_handler(func=lambda message: True)
 async def send_error(message):
 	await bot.send_message(message.chat.id, text='Сообщение не распознано...')
-
 
 if __name__=='__main__':
 	asyncio.run(bot.polling())
--- postgre.py
+++ postgre.py
@@ -1,37 +1,55 @@
-# на текущий момент используется в качестве плейсхолдера
+# Работа с базой данных PostgreSQL
 
-chats = {
-	470922424: [0, ''],
-	458948714: [0, '']
-}
+import psycopg2
+from config import DB_NAME, DB_USER, DB_PASS, DB_HOST
 
-discounts = (
-	(0, 'Обычный, без скидки'),
-	(1, 'Пенсионный, 10% скидка')
-)
+connection = psycopg2.connect(
+	dbname = DB_NAME, 
+	user = DB_USER,
+	password = DB_PASS, 
+	host = DB_HOST)
+
+cursor = connection.cursor()
 
 def get_user_id_for_chat(chat_id):
-	return (chats[chat_id][0] 
-		if chat_id in chats.keys() 
+	cursor.execute("Call get_user_id_by_chat_id (%s, 0)", ([chat_id]))
+	user_id = cursor.fetchall()[0][0]
+	return (user_id 
+		if user_id != -1 
 		else None)
 
 def link_chat_to_user(chat_id, phone_number):
-	chats[chat_id] = [1, '']
+	cursor.execute('''Call add_session (%s, %s)''', ([chat_id, phone_number]))
+	connection.commit()
 
 def unlink_chat_from_user(chat_id):
-	chats.pop(chat_id)
+	cursor.execute("Call delete_session (%s)", ([chat_id]))
+	connection.commit()
 
 def is_admin(user_id):
-	return user_id == 0
+	cursor.execute("Call check_is_admin (%s, False)", ([user_id]))
+	return cursor.fetchall()[0][0]
 
 def get_chat_state(chat_id):
-	return chats[chat_id][1]
+	cursor.execute("Call get_state (%s, '')", ([chat_id]))
+	return cursor.fetchall()[0][0]
 
 def set_chat_state(chat_id, state):
-	chats[chat_id][1] = state
+	cursor.execute("Call change_state (%s, %s)", ([chat_id, state]))
+	connection.commit()
 
 def get_discounts_list():
-	return discounts
+	cursor.execute("Select * from show_discounts")
+	return cursor.fetchall()
 
-def add_user(phone, user_is_admin, discount_id):
-	print(f'USER ADD --- {phone=}, is_admin={user_is_admin}, {discount_id=}')
+def add_user(phone_number, user_is_admin, discount_type_id):
+	cursor.execute("Call add_user (%s, %s, %s)", ([phone_number, discount_type_id, user_is_admin]))
+	connection.commit()
+
+def delete_user (user_id):
+	cursor.execute("Call delete_user (%s)", ([user_id]))
+	connection.commit()
+
+def get_users():
+	cursor.execute ("Select * from show_users")
+	return cursor.fetchall()
